cd0fd00ddcb306e1390b442152350f081f612371
tagawa shouta <sho.webfe@gmail.com>
2021-04-07 01:11:56 +0900

    タグをクリックで検索できるようにした

--- src/components/atoms/PostTag.tsx
+++ src/components/atoms/PostTag.tsx
@@ -1,16 +1,25 @@
+import Link from 'next/link';
+
 interface PostTagProps {
   className?: string,
   withDeleteButton?: boolean,
   value: string,
   onClick?: any,
+  isSearchQuery?: boolean
 }
 
 const PostTag = (props: PostTagProps) => {
-  const { className, withDeleteButton, value, onClick } = props;
+  const { isSearchQuery, className, withDeleteButton, value, onClick } = props;
 
   return (
     <div className={`flex items-center bg-blue-500 text-white mr-2 mb-2 px-2 ${className ? className : ''}`}>
-      {value}
+      {isSearchQuery ? (
+        <Link href={`/search?keyword=${value}`}>
+          <a>
+            {value}
+          </a>
+        </Link>
+      ) : (value)}
       {withDeleteButton && (
         <span
           onClick={onClick}
